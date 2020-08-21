8ebb48bec791e9cc71ad4c196217870105a7d6a1
Aung Ko Ko Thet <aungkoko.thet@mm-digital-solutions.com>
2020-08-22 02:42:39 +0630

    -Number in Kotlin

---
+++ KotlinBasic/app/src/main/java/com/padcmyanmar/kotlinbasic/kotlin/Numbers.kt
@@ -0,0 +1,14 @@
+package com.padcmyanmar.kotlinbasic.kotlin
+
+fun main() {
+
+    var data1 = 0.1
+    var data2 = 1
+
+    data1 = data2.toDouble()
+    data2 = data1.toInt()
+
+    var data3 : Float = 0.1f
+    var data4 : Double = 0.1
+
+}
